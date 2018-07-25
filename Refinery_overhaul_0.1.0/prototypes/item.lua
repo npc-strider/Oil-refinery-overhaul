46fa805bfa921fb92d60073b42df8d17c924f720
npc-strider <npc.strider@gmail.com>
2018-07-25 22:38:14 +0800

    forgot to add renamed dir

---
+++ Refinery_overhaul_0.1.0/prototypes/item.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+	{
+		type = "item",
+		name = "heat-test",
+		icon = "__base__/graphics/icons/chemical-plant.png",
+		icon_size = 32,
+		flags = {"goes-to-quickbar"},
+		subgroup = "production-machine",
+		order = "e[heat-test]",
+		place_result = "heat-test",
+		stack_size = 10
+	}
+})
